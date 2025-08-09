3863a9cae190e647909643a5e7f578e82006a10a
Vinicius Oliveira <viniciusaloli@gmail.com>
2025-08-09 14:12:04 -0300

    commit 1

---
+++ test/login.test.js
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import { expect } from 'chai';
+
+describe('login', () => {
+    describe('POST /login', () => {
+        it('Deve retornar 200 com um token em uma string quando ', async () => {
+            const resposta = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    username:'julio.lima',
+                    senha: '123456'
+                })
+            expect(resposta.status).to.be.equal(200);
+            expect(resposta.body.token).to.be.a('string');
+        })
+    })
+})
