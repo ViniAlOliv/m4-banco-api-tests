3f6777cc098c33a961813df187f0f513454a8234
Vinicius Oliveira <viniciusaloli@gmail.com>
2025-09-09 00:17:46 -0300

    adicionando testes get transferências

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -7,12 +7,13 @@ import postTransferencias from '../fixtures/postTransferencias.json' assert { ty
 dotenv.config();
 
 describe('Transferências', () => {
-    describe('POST /trasferencias', () => {
-        let token;
+    let token;
 
-        beforeEach(async () => {
-            token = await obterToken('julio.lima', '123456')
-        });
+    beforeEach(async () => {
+        token = await obterToken('julio.lima', '123456')
+    });
+
+    describe('POST /trasferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor de transferência for igual ou acima de R$10,00', async () => {
             const bodyTransferencias = { ...postTransferencias};
             
@@ -35,6 +36,32 @@ describe('Transferências', () => {
                     .send(bodyTransferencias)
                 expect(resposta.status).to.be.equal(422);
                 console.log(resposta.body);
+        })
+    })
+
+    describe('GET /transferencias/{id}', () => {
+        it('Deve retornar sucesso com 200 e dados iguais ao registro contido no BD quando ID for válido', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias/5')
+                .set('Authorization', 'Bearer ' + token)
+            console.log(resposta.status);
+            console.log(resposta.body);
+            expect(resposta.status).to.be.equal(200);
+            expect(resposta.body.conta_origem_id).to.equal(1);
+            expect(resposta.body.valor).to.equal(11.00); //Encontramos uma issue. Diff de tipagem de dado
+        })
+    })
+
+    describe('GET /transferencias', () => {
+        it('Deve retornar 10 elementos na paginação', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias')
+                .set('Authorization', 'Bearer ' + token)
+            console.log(resposta.status);
+            console.log(resposta.body);
+            expect(resposta.status).to.be.equal(200);
+            expect(resposta.body.limit).to.equal(10);
+            expect(resposta.body.transferencias).to.have.lengthOf(10);
         })
     })
 })
